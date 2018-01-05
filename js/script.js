16d218c91fcc28a3a68e88b1e318cc1a49dbaa86
Katarzyna Jedrzejczak <katarzyna.jedrzejczak@op.pl>
2018-01-05 23:35:15 +0100

    Change index.html, change sass, change js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+
+var allNames = femaleNames.concat(maleNames);
+console.log(allNames);  
+
+var newName = 'Marian';
+if(allNames.indexOf(newName) === -1) {
+	console.log('The search value is on ' + allNames.indexOf(newName) + ' position');
+	allNames.push(newName);
+	console.log(allNames);
+	console.log('The search value is on ' + allNames.indexOf(newName) + ' position');
+} 
+
+
